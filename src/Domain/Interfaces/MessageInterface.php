95d8f9362b881d61ca76f2cec7fdb0f84c9a4e4e
Carlos Batista <cbatista8a@gmail.com>
2024-06-24 12:58:40 +0200

    [doc] - add comment about features that will be introduced

--- src/Domain/Interfaces/MessageInterface.php
+++ src/Domain/Interfaces/MessageInterface.php
@@ -7,4 +7,6 @@ namespace CubaDevOps\Flexi\Domain\Interfaces;
 interface MessageInterface extends DTOInterface
 {
     public function createdAt(): \DateTimeImmutable;
+
+    //Todo implement different message formats like json, plain text, html, etc.
 }

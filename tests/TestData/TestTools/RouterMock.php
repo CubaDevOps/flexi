c16f6343ef108301776489bdbe64bae5bd9a2e0b
Carlos Batista <cbatista8a@gmail.com>
2024-07-02 18:31:45 +0200

    feat: add spy counter to facilitate some tests
    Signed-off-by: Carlos Batista <cbatista8a@gmail.com>

--- tests/TestData/TestTools/RouterMock.php
+++ tests/TestData/TestTools/RouterMock.php
@@ -2,6 +2,7 @@
 
 namespace CubaDevOps\Flexi\Test\TestData\TestTools;
 
+use CubaDevOps\Flexi\Domain\Classes\Route;
 use CubaDevOps\Flexi\Domain\Classes\Router;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -9,6 +10,7 @@ use Psr\Http\Message\ServerRequestInterface;
 class RouterMock extends Router
 {
     public bool $redirect_to_not_found_spy = false;
+    public int $route_counter = 0;
 
     public function redirectToNotFound(
         ServerRequestInterface $request,
@@ -17,5 +19,12 @@ class RouterMock extends Router
         $response = $this->response_factory->createResponse(404);
         $this->redirect_to_not_found_spy = true;
         return $response;
+    }
+
+    public function addRoute(Route $route): Router
+    {
+        parent::addRoute($route);
+        $this->route_counter++;
+        return $this;
     }
 }

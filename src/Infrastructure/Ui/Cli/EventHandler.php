8ab2a80b90ff4dfabba07a2ccaeea107b21862d5
Carlos Batista <cbatista8a@gmail.com>
2025-03-09 18:31:23 +0100

    refactor (review): moves the TriggerEvent class to the console scope

---
+++ src/Infrastructure/Ui/Cli/EventHandler.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CubaDevOps\Flexi\Infrastructure\Ui\Cli;
+
+use CubaDevOps\Flexi\Domain\Classes\Event;
+use CubaDevOps\Flexi\Domain\Interfaces\EventBusInterface;
+
+class EventHandler
+{
+
+    private EventBusInterface $event_bus;
+
+    public function __construct(EventBusInterface $event_bus)
+    {
+        $this->event_bus = $event_bus;
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    public function handle(CliInput $input): string
+    {
+        if ($input->showHelp()) {
+            return 'Usage: --event|-e trigger|listeners name=event_name fired_by=cli data={"key": "value"}';
+        }
+
+        if ($input->getCommandName() === 'trigger') {
+            $data = json_decode((string)$input->getArgument('data', '{}'), true, 512, JSON_THROW_ON_ERROR);
+            $event = new Event($input->getArgument('name'), $input->getArgument('fired_by', 'cli'), $data);
+            $this->event_bus->dispatch($event);
+            return 'Event triggered';
+        }
+
+        if ($input->getCommandName() === 'listeners') {
+            $listeners = $this->event_bus->getListeners($input->getArgument('name'));
+            return implode("\n", $listeners);
+        }
+
+        return $input->getCommandName() . ' command related to events not found';
+    }
+}

498de73c8d0fd99ba439a38cdb1d9be687326eba
Carlos Batista <59052959+cbatista8a@users.noreply.github.com>
2025-04-05 19:26:30 +0200

    feat: container refactor to lazy loading (#56)
    * feat: implements a lazy loading container with cache
    
    * fix: update the tests with the latest changes

--- src/Infrastructure/DependencyInjection/Container.php
+++ src/Infrastructure/DependencyInjection/Container.php
@@ -14,8 +14,6 @@ use Psr\Container\NotFoundExceptionInterface;
 
 class Container implements ContainerInterface
 {
-    use CacheKeyGeneratorTrait;
-
     private const CONTAINER_CACHE_KEY = 'container';
     private const SERVICE_CACHE_KEY_PREFIX = 'service.';
     private const SERVICE_DEFINITIONS_KEY = 'service_definitions';
@@ -78,19 +76,11 @@ class Container implements ContainerInterface
      *
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
-     * @throws \ReflectionException
-     * @throws InvalidArgumentException
      */
     public function get(string $id): object
     {
         if (in_array($id, $this->selfReference, true)) {
             return $this;
-        }
-        if ('cache' === $id || CacheInterface::class === $id) {
-            return $this->cache;
-        }
-        if (ClassFactory::class === $id) {
-            return $this->factory;
         }
 
         $cacheKey = $this->generateServiceCacheKey($id);
@@ -114,10 +104,6 @@ class Container implements ContainerInterface
      *
      * @param string $id
      * @return object
-     * @throws NotFoundExceptionInterface
-     * @throws \ReflectionException
-     * @throws ContainerExceptionInterface
-     * @throws InvalidArgumentException
      */
     private function resolveServiceInstance(string $id): object
     {

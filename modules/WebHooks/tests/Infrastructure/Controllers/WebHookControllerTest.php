3445bc382aae23301e18171812aff46956af93b1
Carlos Batista <59052959+cbatista8a@users.noreply.github.com>
2025-10-28 17:28:18 +0100

    fix: Update middleware reference in routes and add test for middleware handling

--- modules/WebHooks/tests/Infrastructure/Controllers/WebHookControllerTest.php
+++ modules/WebHooks/tests/Infrastructure/Controllers/WebHookControllerTest.php
@@ -6,6 +6,7 @@ namespace CubaDevOps\Flexi\Modules\WebHooks\Test\Infrastructure\Controllers;
 
 use CubaDevOps\Flexi\Domain\Events\Event;
 use CubaDevOps\Flexi\Infrastructure\Bus\EventBus;
+use CubaDevOps\Flexi\Modules\Auth\Infrastructure\Middlewares\JWTAuthMiddleware;
 use CubaDevOps\Flexi\Modules\WebHooks\Infrastructure\Controllers\WebHookController;
 use CubaDevOps\Flexi\Test\TestData\TestDoubles\DummyResponseFactory;
 use PHPUnit\Framework\TestCase;
@@ -82,6 +83,30 @@ class WebHookControllerTest extends TestCase
         $response = $this->webHookController->handle($this->requestMock);
 
         $this->assertEquals(400, $response->getStatusCode());
+    }
+
+    public function testHandlerWithMiddlewares(): void
+    {
+        $middleware = $this->createMock(JWTAuthMiddleware::class);
+        $this->webHookController->addMiddleware($middleware);
+        $data = [
+            'event' => 'test_event',
+            'fired_by' => 'tester',
+            'data' => ['key' => 'value'],
+        ];
+        $this->mockRequestPayload($data);
+        $middleware
+            ->expects($this->once())
+            ->method('process')
+            ->willReturnCallback(function ($request, $handler) {
+                return $handler->handle($request);
+            });
+        $this->eventBusMock
+            ->expects($this->once())
+            ->method('dispatch')
+            ->with($this->isInstanceOf(Event::class));
+        $response = $this->webHookController->handle($this->requestMock);
+        $this->assertEquals(200, $response->getStatusCode());
     }
 
     private function mockRequestPayload(array $payload): void
